5612eb5e0cc14e090021f821492927e9a5693ec5
Ramakrishna chennuri <ramakrishna.chennuri@archents.com>
2024-02-10 18:03:42 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { PortfolioComponent } from '../components/portfolio/portfolio.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,PortfolioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
